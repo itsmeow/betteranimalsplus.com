fb3ae86ddc49160b78e2424e79fe6327670d3fc3
itsmeow <mods.itsmeow@gmail.com>
2020-12-02 20:07:18 -0500

    Fix SSR and update staff logos + add border to logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ import StaffCard from "../components/staffCard";
 import itsmeowLogo from "../data/logos/itsmeow.png";
 import cybercatLogo from "../data/logos/cybercat5555.png";
 import batLogo from "../data/logos/batpixxler.png";
+import ClientOnly from "../components/clientonly";
 
 const IndexPage = () => {
   return (
@@ -43,61 +44,63 @@ const IndexPage = () => {
         />
       </div>
       <main>
-        <PageSection animateIn="animate__zoomInDown">
-          <div className="text-center medium">With Over</div>
-          <DownloadOdometer initialValue="1000000" />
-          <div className="text-center medium">Downloads</div>
-        </PageSection>
-        <PageSection animateIn="animate__bounceInRight">
-          <div className="text-center medium">Built by</div>
-          <div className="limit-width">
-            <StaffCard
-              name="cybercat5555"
-              role="Lead Artist and Designer"
-              image={cybercatLogo}
-              style={{ marginBottom: "20px" }}
-              className="center-item-2"
-            >
-              Cybercat is responsible for creating models, textures, ideas, and
-              is the lead designer for the mod. Cyber created Better Animals
-              Plus and built the team responsible.
-            </StaffCard>
-            <StaffCard
-              name="itsmeow"
-              role="Lead Programmer"
-              image={itsmeowLogo}
-              style={{ marginBottom: "20px" }}
-              className="center-item-2"
-              links={
-                <>
-                  <LinkButton
-                    link="https://itsmeow.dev/"
-                    color="rgb(25, 144, 255)"
-                    hoverColor="rgb(0, 132, 255)"
-                    style={{ transitionDuration: "300ms" }}
-                  >
-                    Website
-                  </LinkButton>
-                  <PatreonButton className="devlinkbutton" />
-                </>
-              }
-            >
-              As the lead and sole Programmer, itsmeow is responsible for
-              creating the AI, items, blocks, and code required to make the mod
-              function, as well as adding complimentary features such as
-              configuration.
-            </StaffCard>
-            <StaffCard
-              name="Batpixxler"
-              role="Artist and Designer"
-              image={batLogo}
-              className="center-item-2"
-            >
-              Batpixxler, otherwise known as Batman, assists Cyber in design and
-              makes art for Better Animals Plus.
-            </StaffCard>
-          </div>
-        </PageSection>
+        <ClientOnly>
+          <PageSection animateIn="animate__zoomInDown">
+            <div className="text-center medium">With Over</div>
+            <DownloadOdometer initialValue="1000000" />
+            <div className="text-center medium">Downloads</div>
+          </PageSection>
+          <PageSection animateIn="animate__bounceInRight">
+            <div className="text-center medium">Built by</div>
+            <div className="limit-width">
+              <StaffCard
+                name="cybercat5555"
+                role="Lead Artist and Designer"
+                image={cybercatLogo}
+                style={{ marginBottom: "20px" }}
+                className="center-item-2"
+              >
+                Cybercat is responsible for creating models, textures, ideas,
+                and is the lead designer for the mod. Cyber created Better
+                Animals Plus and built the team responsible.
+              </StaffCard>
+              <StaffCard
+                name="itsmeow"
+                role="Lead Programmer"
+                image={itsmeowLogo}
+                style={{ marginBottom: "20px" }}
+                className="center-item-2"
+                links={
+                  <>
+                    <LinkButton
+                      link="https://itsmeow.dev/"
+                      color="rgb(25, 144, 255)"
+                      hoverColor="rgb(0, 132, 255)"
+                      style={{ transitionDuration: "300ms" }}
+                    >
+                      Website
+                    </LinkButton>
+                    <PatreonButton className="devlinkbutton" />
+                  </>
+                }
+              >
+                As the lead and sole Programmer, itsmeow is responsible for
+                creating the AI, items, blocks, and code required to make the
+                mod function, as well as adding complimentary features such as
+                configuration.
+              </StaffCard>
+              <StaffCard
+                name="Batpixxler"
+                role="Artist and Designer"
+                image={batLogo}
+                className="center-item-2"
+              >
+                Batpixxler, otherwise known as Batman, assists Cyber in design
+                and makes art for Better Animals Plus.
+              </StaffCard>
+            </div>
+          </PageSection>
+        </ClientOnly>
       </main>
     </Layout>
   );
